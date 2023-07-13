5d33d65cb1cf3704dd823371252f88bfed51822b
Miamura <120501241+uzbek69@users.noreply.github.com>
2023-07-13 10:53:01 +0300

    Add files via upload

---
+++ db_scripts.py
@@ -0,0 +1,52 @@
+import sqlite3
+db_name = 'quiz.sqlite'
+conn = None
+curor = None
+
+def open():
+    global conn, cursor
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+def close():
+    cursor.close()
+    conn.close()
+
+def do(query):
+    cursor.execute(query)
+    conn.commit()
+
+def clear_db():
+    ''' удаляет все таблицы '''
+    open()
+    query = '''DROP TABLE IF EXISTS quiz_content'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS question'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS quiz'''
+    do(query)
+    close()
+
+    
+def create():
+    pass
+
+def show(table):
+    query = 'SELECT * FROM ' + table
+    open()
+    cursor.execute(query)
+    print(cursor.fetchall())
+    close()
+
+def show_tables():
+    show('question')
+    show('quiz')
+    show('quiz_content')
+
+def main():
+    clear_db()
+    create()
+    show_tables()
+
+if __name__ == "__main__":
+    main()
